6a4bb514be9dcc70bb3feb516ffbdd9da467a004
dude-guy-boy <dude0guy0boy@gmail.com>
2023-06-01 21:44:03 +1000

    Added function to get name from uuid also

--- src/mojang.py
+++ src/mojang.py
@@ -2,7 +2,7 @@ import requests
 
 class MojangAPI:
     @classmethod
-    def get_uuid_from_name(self, name: str):
+    def get_uuid_from_name(cls, name: str):
         req = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{name}")
 
         try:
@@ -11,3 +11,13 @@ class MojangAPI:
             return None
 
         return req.json()['id']
+    
+    @classmethod
+    def get_name_from_uuid(cls, uuid: str):
+        req = requests.get(f"https://api.mojang.com/user/profile/{uuid}")
+        try:
+            req.json()
+        except:
+            return None
+
+        return req.json()['name']
