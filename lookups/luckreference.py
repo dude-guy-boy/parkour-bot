ec582c2101d2d9e254a105db2130e1959abb564c
dude-guy-boy <dude0guy0boy@gmail.com>
2023-06-01 17:55:46 +1000

    Added reference for each luck

---
+++ lookups/luckreference.py
@@ -0,0 +1,51 @@
+powers = {
+        3: {
+            "chance": 1000,
+            "text": "a thousand",
+            "reactions": ["🍀"],
+            "replies": [],
+            "points": 1
+        },
+        4: {
+            "chance": 10000,
+            "text": "ten thousand",
+            "reactions": ["🏆", "👹"],
+            "replies": ["ai"],
+            "points": 10
+        },
+        5: {
+            "chance": 100000,
+            "text": "a hundred thousand",
+            "reactions": ["👀", "🧐", "😶", "🐸"],
+            "replies": ["wtf", "ai"],
+            "points": 100
+        },
+        6: {
+            "chance": 1000000,
+            "text": "a million",
+            "reactions": ["👀", "🧐", "😶‍🌫️", "🐸", "🏆", "👹", "🍀"],
+            "replies": ["ai", "ai"],
+            "points": 1000
+        },
+        7: {
+            "chance": 10000000,
+            "text": "ten million",
+            "reactions": [":Gerald:866042008997330975", ":Dan:840337129330704454", ":Benjamaster7:798824200369733663", ":BRClowny:1000626116291207228", ":Yo:794085095325433927", ":letsfuckinggo:1002598883186188369", "🍀"],
+            "replies": ["Congratulations. You have received a reaction that has a 1 in 10 million chance of happening. How rare is that exactly? Think about a collection of 1000 items. Then, think about a collection containing each 1000 item collection 1000 times. Now think about 10 of all of those. That's how rare it is. So... celebrate? I guess? Is it really something to celebrate though? What feat did you accomplish exactly? You got a reaction and a paragraph from me? Think about how rare 1 in 10 million is. What extraordinary things could be accomplished for the world that have a 1 in 10 million chance of happening? And you just wasted it on a Discord message? Do you not even realize how sad that is? But for now, let's hypothetically assume that sending a ton of Discord message is the best thing you can do for humanity. Even so, do you deserve this reward? Have you personally sent 10 million messages to the server? Or are you just a lucky ass? Instead of being happy about this reward, I encourage you to really think about what has happened and think about what better things you could have done with your life than that.", "ai", "ai", "ai"],
+            "points": 10000
+        },
+        8: {
+            "chance": 100000000,
+            "text": "a hundred million",
+            "reactions": ["👀", "🧐", "😶‍🌫️", "🐸", "🏆", "👹", "🍀", ":Gerald:866042008997330975", ":Dan:840337129330704454", ":Benjamaster7:798824200369733663", ":BRClowny:1000626116291207228", ":Yo:794085095325433927", ":letsfuckinggo:1002598883186188369"],
+            "replies": ["ai", "ai", "ai", "ai", "ai"],
+            "points": 100000
+        },
+        9: {
+            "chance": 1000000000,
+            "text": "a billion",
+            "reactions": ["🦑"],
+            "replies": ["ai", "ai", "ai", "ai", "ai", "ai", "ai", "ai", "ai"],
+            "points": 1000000
+        }
+    }
