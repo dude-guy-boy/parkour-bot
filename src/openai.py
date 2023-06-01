a951aab92902fd3465ac15f55f2fb7298215df8a
dude-guy-boy <dude0guy0boy@gmail.com>
2023-06-01 17:56:43 +1000

    finished openai.py

--- src/openai.py
+++ src/openai.py
@@ -1,6 +1,7 @@
 import openai, os
 from dotenv import load_dotenv
 from typing import List
+import src.logs as logs
 
 class Role:
     USER = "user"
@@ -13,30 +14,36 @@ class Message:
 
 class OpenAI:
     __api_key = None
+    logger = None
 
     def __load_api_key(self):
         if not self.__api_key:
             load_dotenv()
             self.__api_key = os.environ.get("OPENAI_KEY")
+            self.logger = logs.init_logger()
 
     @classmethod
     def chat_response(cls, messages: List[Message], max_tokens: int = 50):
         cls.__load_api_key(cls)
+        cls.logger.info("Making AI Chat Request")
 
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[{"role": message.role, "content": message.content} for message in messages],
-            max_tokens = 150,
+            max_tokens = max_tokens,
             api_key = cls.__api_key
         )
 
-        print(response)
+        generated_text = response.choices[0]['message']['content']
+        if generated_text.startswith('"') and generated_text.endswith('"'):
+            generated_text = generated_text[1:-1]
 
-        return response.choices[0]['message']['content']
+        return generated_text
 
     @classmethod
     def moderation(cls, input):
         cls.__load_api_key(cls)
+        cls.logger.info("Making AI Moderation Request")
 
         response = openai.Moderation.create(
             input=input,
