f23171540ad3438e84668bcd185ab469f53fb766
dude-guy-boy <dude0guy0boy@gmail.com>
2023-06-02 16:50:18 +1000

    Added bot update command

--- extensions/admin/manager.py
+++ extensions/admin/manager.py
@@ -17,12 +17,21 @@ from src.database import Config, Data
 from os import execl, kill
 from sys import executable, argv
 from signal import SIGKILL
+from git import Repo
 
 class Manager(Extension):
     def __init__(self, client: Client):
         self.client = client
         self.logger = logs.init_logger()
+        self.repo = Repo('.')
+        assert not self.repo.bare
 
+    # TODO: Add management commands
+    # Force backup
+    # Get backup
+    # Get logs
+
+    ### /BOT RESTART ###
     @slash_command(
         name="bot",
         description="base bot command",
@@ -30,6 +39,45 @@ class Manager(Extension):
         sub_cmd_description="Restarts the bot"
     )
     async def bot_restart(self, ctx: SlashContext):
+        await self.restart(ctx)
+
+    ### /BOT UPDATE ###
+    @slash_command(
+        name="bot",
+        description="base bot command",
+        sub_cmd_name="update",
+        sub_cmd_description="Updates the bot"
+    )
+    async def bot_update(self, ctx: SlashContext):
+        await ctx.defer()
+
+        try:
+            # Do the pull
+            git = self.repo.git
+            pull = git.pull()
+
+            # Format the pull
+            split_string = pull.split('\n')
+            formatted_string = ''
+            for line in split_string:
+                line = line.lstrip()
+                formatted_string = f"{formatted_string}\n> {line}"
+
+            # Send updates
+            await ctx.send(embeds=Embed(description=f"**Pulling Repository:**{formatted_string}", color=Color.GREEN))
+
+            # Log the update
+            # TODO: discord logger
+            # await self.discord_logger.log_command(ctx, f"Pulled repository.{formatted_string}")
+            reformatted = formatted_string.replace('\n', ':')
+            self.logger.info(f"Pulled repository. {reformatted}")
+            await self.restart(ctx)
+        except:
+            await ctx.send(embeds=Embed(description="Failed to pull updates", color=Color.RED))
+            self.logger.warning("Failed to pull repository")
+            await self.discord_logger.log_command(ctx, f"Failed to pull repository")
+
+    async def restart(self, ctx: SlashContext):
         # Set channel where message should be sent on startup
         Data.set_data_item("restart_channel", str(ctx.channel.id))
         await ctx.send(embeds=Embed(description="Restarting...", color=Color.YORANGE))
@@ -41,14 +89,6 @@ class Manager(Extension):
 
         # Restarts bot
         execl(executable, *([executable]+argv))
-
-    # TODO: Add management commands
-    # Bot restart
-    # Bot reload
-    # Bot update
-    # Force backup
-    # Get backup
-    # Get logs
 
 def setup(bot):
     # This is called by interactions.py so it knows how to load the Extension
