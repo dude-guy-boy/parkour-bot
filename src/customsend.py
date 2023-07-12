8ab50918da8fda3bd2d4b9e0d0ddf22486801635
dude-guy-boy <dude0guy0boy@gmail.com>
2023-07-12 21:12:07 +1000

    Added to_channel optional argument for custom send, and added custom edit also

--- src/customsend.py
+++ src/customsend.py
@@ -1,5 +1,11 @@
-from interactions import Embed, InteractionContext
+from interactions import Embed, InteractionContext, Message
 from lookups.colors import Color
 
-async def send(ctx: InteractionContext, content: str, color: int = Color.GREEN, ephemeral: bool = False):
+async def send(ctx: InteractionContext, content: str, color: int = Color.GREEN, ephemeral: bool = False, to_channel: bool = False):
+    if to_channel:
+        return await ctx.channel.send(embed=Embed(description=content, color=color))
+    
     return await ctx.send(embed=Embed(description=content, color=color), ephemeral=ephemeral)
+
+async def edit(message: Message, content: str, color: int = Color.GREEN):
+    return await message.edit(embed=Embed(description=content, color=color))
